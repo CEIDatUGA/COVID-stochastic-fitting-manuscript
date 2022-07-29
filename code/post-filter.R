fb1d6d81b8742e8ef2fe12432db8e59a0b3a2ed2
Andrew Tredennick <atredennick@WEST-INC.LOCAL>
2022-07-29 08:57:33 -0600

    Adds filtered states for fitted locations

--- code/post-filter.R
+++ code/post-filter.R
@@ -8,10 +8,8 @@ library(pomp)  # must be v3.1
 # devtools::install_version("pomp", version = "3.1", repos = "http://cran.us.r-project.org")
 library(foreach)
 library(doParallel)
-library(parallel)
 
-PF_REPS <- 500
-PF_PARTICLES <- 2500
+
 NUM_CORES <- 17
 
 
@@ -20,13 +18,16 @@ states_to_filter <- state.name[1:17]  # states with fits but no filters
 mif_path <- "L:/1186 - UGA/MIFS/"
 all_mif_files <- list.files(mif_path)[grep("res_2", list.files(mif_path))]
 
-registerDoParallel(NUM_CORES)
+cl <- makeCluster(NUM_CORES)
+registerDoParallel(cl)
 foreach(i = 1:length(states_to_filter), 
         .packages = c("pomp", "dplyr", "tibble", "tidyr")) %dopar% {
+          dir.create(paste0("../", i))
+          setwd(paste0("../", i))
           # load fit
           fname <- all_mif_files[grep(states_to_filter[i], all_mif_files)]
           pomp_res2 <- readRDS(paste0(mif_path, fname))
-          
+
           # extract MLEs
           params <- pomp_res2$all_partable %>%
             arrange(-LogLik) %>%
@@ -34,14 +35,23 @@ foreach(i = 1:length(states_to_filter),
             dplyr::select(-MIF_ID, -LogLik, -LogLik_SE) %>%
             tidyr::gather() %>%
             tibble::deframe()
-          
+
+          # reset pomp model
+          source("../code/model-setup/makepompmodel.R")
+          pomp_model <- makepompmodel(
+            par_var_list = pomp_res2$par_var_list,
+            pomp_data = pomp_res2$pomp_data,
+            pomp_covar = pomp_res2$pomp_covar,
+            n_knots = length(grep("seas", rownames(pomp_res2$pomp_covar@table)))
+          )
+
           out <- tibble::tibble()
-          for(j in 1:PF_REPS){
+          for(j in 1:500){
             # run filter
             pf <- pfilter(
-              pomp_res2$pomp_model,
+              pomp_model,
               params = params,
-              Np = PF_PARTICLES,
+              Np = 2500,
               filter.traj = TRUE,
               save.states = TRUE,
               max.fail = Inf
@@ -59,4 +69,4 @@ foreach(i = 1:length(states_to_filter),
           }
           saveRDS(out, paste0("../output/",state.name[i],"-filtered_states.RDS"))
         }
-stopImplicitCluster()
+stopCluster(cl)
